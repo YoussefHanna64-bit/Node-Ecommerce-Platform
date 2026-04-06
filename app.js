c75a62abb7251475301119a8629cbdd833429451
YoussefHanna64-bit <JCBIOX200@GMAIL.COM>
2026-04-06 18:36:04 +0200

    Initial project structure

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/dbConfig.js";
+
+dotenv.config();
+
+connectDB();
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log("server is running");
+});
