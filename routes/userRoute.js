a3284073a403b32d6fc104881e5d2e1e353f45e3
Ali Azouz <aliazouz.808@gmail.com>
2026-04-08 17:05:47 +0200

    Modify the user route authorization

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,14 +6,12 @@ import {
     deleteUser
 } from '../controllers/userController.js';
 import verifyToken from '../middlewares/verifyToken.js';
-import authorize from '../middlewares/authorize.js';
-
 
 const router = express.Router()
 
-router.get("/", verifyToken, authorize("admin"), getAllUsers)
-router.get("/:id", verifyToken, authorize("admin"), getUserById)
-router.patch("/:id", verifyToken, authorize("admin"), updateUser)
-router.delete("/:id", verifyToken, authorize("admin"), deleteUser)
+router.get("/", verifyToken, getAllUsers)
+router.get("/:id", verifyToken, getUserById)
+router.patch("/:id", verifyToken, updateUser)
+router.delete("/:id", verifyToken, deleteUser)
 
 export default router;
